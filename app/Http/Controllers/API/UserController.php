22764fd35c7aa45bb49e0b769c590a48cf321386
appdevina <devina@completeselular.com>
2023-01-31 10:08:06 +0700

    api login & fetch user

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+use Faker\Guesser\Name;
+
+namespace App\Http\Controllers\API;
+
+use Exception;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller{
+    public function login(Request $request)
+    {
+       try {
+            $request->validate([
+                'username' => 'required',
+                'password' => 'required'
+            ]);
+
+            $credentials = request(['username','password']);
+            if (!Auth::attempt($credentials)) {
+                return ResponseFormatter::error([
+                    'message' => 'Unauthorized'
+                ],  'Authentication Failed', 500
+            );
+            }
+
+            $user = User::where('username', $request->username)->first();
+
+            if(!Hash::check($request->password, $user->password, [])){
+                throw new \Exception('Invalid Credentials');
+            }
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ],  'Authenticated'
+            );
+
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                //'error' => $error
+            ],  'Authentication Failed', 500
+        );
+        $error->getMessage();
+       } 
+    }
+
+    public function fetch(Request $request)
+    {
+        return ResponseFormatter::success($request->user(),'Data profile user berhasil diambil');
+    }
+}
--- resources/views/layouts/includes/_sidebar.blade.php
+++ resources/views/layouts/includes/_sidebar.blade.php
@@ -4,7 +4,7 @@
 			<ul class="nav">
 				<li><a href="/dashboard" class="active"><i class="lnr lnr-home"></i> <span>DASHBOARD</span></a></li>
 				<li><a href="#" class=""><i class="lnr lnr-cart"></i> <span>REQUEST</span></a></li>
-                @if (auth()->user()->role == 1)
+                @if (auth()->user()->role_id == 1)
 				<li>
 					<a href="#subPagesMaster" data-toggle="collapse" class="collapsed"><i class="lnr lnr-list"></i> <span>MASTER DATA</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
 					<div id="subPagesMaster" class="collapse ">
